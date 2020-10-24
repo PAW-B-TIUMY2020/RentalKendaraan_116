37a43095aa7c4dfe2bb870acfb11032fa7f25654
Bima Arya Saputra Wibowo <62773300+Aeriya2@users.noreply.github.com>
2020-10-24 13:47:33 +0700

    Add project files.

---
+++ Models/Costumer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RentalKendaraan_116.Models
+{
+    public partial class Costumer
+    {
+        public Costumer()
+        {
+            Peminjaman = new HashSet<Peminjaman>();
+        }
+
+        public int IdCostumer { get; set; }
+        public string NamaCostumer { get; set; }
+        public string Nik { get; set; }
+        public string Alamat { get; set; }
+        public string NoHp { get; set; }
+        public int? IdGender { get; set; }
+
+        public Gender IdGenderNavigation { get; set; }
+        public ICollection<Peminjaman> Peminjaman { get; set; }
+    }
+}
