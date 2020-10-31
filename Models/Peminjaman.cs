d27b3c3d20fcb2464441e8391d37c0ce17eca308
Bima Arya Saputra Wibowo <62773300+Aeriya2@users.noreply.github.com>
2020-10-31 12:23:30 +0700

    dropdown

--- Models/Peminjaman.cs
+++ Models/Peminjaman.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace RentalKendaraan_116.Models
 {
@@ -9,16 +11,24 @@ namespace RentalKendaraan_116.Models
         {
             Pengembalian = new HashSet<Pengembalian>();
         }
-
+        [DisplayName("ID")]
         public int IdPeminjaman { get; set; }
+        [DisplayName("Tanggal Peminjaman")]
         public DateTime? TglPeminjaman { get; set; }
+        [DisplayName("Kendaraan")]
         public int? IdKendaraan { get; set; }
+        [DisplayName("Costumer")]
         public int? IdCostumer { get; set; }
+        [DisplayName("Jaminan")]
         public int? IdJaminan { get; set; }
+        [DisplayName("Biaya")]
         public int? Biaya { get; set; }
 
+        [DisplayName("Costumer")]
         public Costumer IdCostumerNavigation { get; set; }
+        [DisplayName("Jaminan")]
         public Jaminan IdJaminanNavigation { get; set; }
+        [DisplayName("Kendaraan")]
         public Kendaraan IdKendaraanNavigation { get; set; }
         public ICollection<Pengembalian> Pengembalian { get; set; }
     }
